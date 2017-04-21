fcc88a76d4cc4951737863044cdfcf9677c951a9
Otavios <otavio.cts@hotmail.com>
2017-04-21 00:41:19 -0300

    Comentários adicionados

--- main.cpp
+++ main.cpp
@@ -8,34 +8,39 @@ using namespace std;
 
 int main(){
 	
-	int largura = 960;
-	int altura = 540;
-	bool acabou = false;
+	// declaração de variáveis
+	int largura = 960; // largura da tela
+	int altura = 540; // altura da tela
+	bool acabou = false; // variavel que controla o loop do jogo
 	
+	// declaração de variáveis da allegro
 	ALLEGRO_DISPLAY *tela = NULL;
 	ALLEGRO_EVENT_QUEUE *filaEventos = NULL;
 	
-	al_init();
+	al_init(); // inicializa a allegro
 	
-	tela = al_create_display(largura, altura);
+	tela = al_create_display(largura, altura); //cria a janela
 	
-	filaEventos = al_create_event_queue();
+	filaEventos = al_create_event_queue(); //cria a fila de eventos
 	
+	// inicialização dos addons
 	al_install_keyboard();
 	
+	// registra fontes de eventos na fila de eventos
 	al_register_event_source(filaEventos, al_get_display_event_source(tela));
 	al_register_event_source(filaEventos, al_get_keyboard_event_source());
 	
+	// loop do jogo
 	while(!acabou){
 		
-		ALLEGRO_EVENT evento;
-		al_wait_for_event(filaEventos, &evento);
+		ALLEGRO_EVENT evento; // cria variável de evento
+		al_wait_for_event(filaEventos, &evento); // espera por algum evento
 		
-		if(evento.type == ALLEGRO_EVENT_DISPLAY_CLOSE){
+		if(evento.type == ALLEGRO_EVENT_DISPLAY_CLOSE){ // evento disparado quando clicamos no X da tela
 			acabou = true;
 		}
-		else if(evento.type == ALLEGRO_EVENT_KEY_DOWN){
-			switch(evento.keyboard.keycode){
+		else if(evento.type == ALLEGRO_EVENT_KEY_DOWN){ // evento disparado quando uma tecla é pressionada
+			switch(evento.keyboard.keycode){ // verifica qual tecla foi pressionada
 				case ALLEGRO_KEY_ESCAPE:
 					acabou = true;
 					break;
